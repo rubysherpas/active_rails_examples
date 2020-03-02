7025309b47c39b2051edbda3bb230860f35a261c
Ryan Bigg <me@ryanbigg.com>
2020-03-02 13:53:03 +1100

    Projects can now be deleted

--- ticketee/app/controllers/projects_controller.rb
+++ ticketee/app/controllers/projects_controller.rb
@@ -37,6 +37,15 @@ class ProjectsController < ApplicationController
       flash.now[:alert] = "Project has not been updated."
       render "edit"
     end
+  end
+
+  def destroy
+    @project = Project.find(params[:id])
+    @project.destroy
+
+    flash[:notice] = "Project has been deleted."
+    redirect_to projects_path
+  end
 
   private
 
