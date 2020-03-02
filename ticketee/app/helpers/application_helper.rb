bf5f1186995b7a78f9a43f7a02e6556d73ea6a98
Ryan Bigg <me@ryanbigg.com>
2020-03-02 16:36:11 +1100

    More tailwind styles

--- ticketee/app/helpers/application_helper.rb
+++ ticketee/app/helpers/application_helper.rb
@@ -6,4 +6,10 @@ module ApplicationHelper
       end
     end
   end
+
+  def error_messages_for(object, field)
+    content_tag(:p, class: "text-red-500 text-xs italic mt-2") do
+      object.errors.full_messages_for(field).first
+    end
+  end
 end
