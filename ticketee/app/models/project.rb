2c70749cf2035d00b8f91f5e18c5d3587dcf7931
Kieran Andrews <hello@kieranandrews.com.au>
2020-03-11 23:05:07 +1030

    Implement creating tickets for a project

--- ticketee/app/models/project.rb
+++ ticketee/app/models/project.rb
@@ -1,3 +1,4 @@
 class Project < ApplicationRecord
+  has_many :tickets
   validates :name, presence: true
 end
--- ticketee/config/routes.rb
+++ ticketee/config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root "projects#index"
 
-  resources :projects
+  resources :projects do
+    resources :tickets
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
