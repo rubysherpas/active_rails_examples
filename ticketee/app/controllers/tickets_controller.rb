a48e96eb25b1960adce43584e29e49bec34425a7
Kieran Andrews <hello@kieranandrews.com.au>
2020-03-17 19:53:30 +1030

    Tickets can now be edited

--- ticketee/app/controllers/tickets_controller.rb
+++ ticketee/app/controllers/tickets_controller.rb
@@ -20,6 +20,19 @@ class TicketsController < ApplicationController
 
   def show
   end
+
+  def edit
+  end
+
+  def update
+    if @ticket.update(ticket_params)
+      flash[:notice] = "Ticket has been updated."
+      redirect_to [@project, @ticket]
+    else
+      flash.now[:alert] = "Ticket has not been updated."
+      render "edit"
+    end
+  end
   
   private
 
