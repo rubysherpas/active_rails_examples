de95829d97da716ef66c12075dbccea8f73d830b
Kieran Andrews <hello@kieranandrews.com.au>
2020-04-12 10:44:04 +0930

    Add admin namespace with application controller

--- ticketee/config/routes.rb
+++ ticketee/config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    root "application#index"
+  end
   devise_for :users
   root "projects#index"
 
--- ticketee/spec/rails_helper.rb
+++ ticketee/spec/rails_helper.rb
@@ -62,5 +62,6 @@ RSpec.configure do |config|
   # config.filter_gems_from_backtrace("gem name")
 
   config.include Warden::Test::Helpers, type: :feature
+  config.include Devise::Test::ControllerHelpers, type: :controller
   config.after(type: :feature) { Warden.test_reset! }
 end
