aee7ba8c65f35fc5237647668de0d201d2a44f54
Kieran Andrews <hello@kieranandrews.com.au>
2020-05-13 18:22:11 +0930

    Add the ability to attach a file to a ticket

--- ticketee/app/controllers/tickets_controller.rb
+++ ticketee/app/controllers/tickets_controller.rb
@@ -9,7 +9,6 @@ class TicketsController < ApplicationController
   def create
     @ticket = @project.tickets.build(ticket_params)
     @ticket.author = current_user
-  
     if @ticket.save
       flash[:notice] = "Ticket has been created."
       redirect_to [@project, @ticket]
@@ -45,7 +44,7 @@ class TicketsController < ApplicationController
   private
 
   def ticket_params
-    params.require(:ticket).permit(:name, :description)
+    params.require(:ticket).permit(:name, :description, :attachment)
   end
 
   def set_project
