d21a7fa1fdbd5bdf78d6b00f12a74469f3111392
Ryan Bigg <me@ryanbigg.com>
2021-01-10 21:10:39 +1100

    Added bootstrap for styling

--- ticketee/app/javascript/packs/application.js
+++ ticketee/app/javascript/packs/application.js
@@ -3,11 +3,14 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
+import Rails from "@rails/ujs";
+import Turbolinks from "turbolinks";
+import * as ActiveStorage from "@rails/activestorage";
+import "channels";
+require("jquery");
+require("popper.js");
+require("bootstrap");
 
-Rails.start()
-Turbolinks.start()
-ActiveStorage.start()
+Rails.start();
+Turbolinks.start();
+ActiveStorage.start();
