88142a9850bb09a0c7aab80d5cf8c02555c2190f
Ryan Bigg <me@ryanbigg.com>
2021-01-15 15:33:03 +1100

    Add the ability to create admin users through the admin backend

--- ticketee/app/models/user.rb
+++ ticketee/app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def to_s
+    "#{email} (#{admin? ? "Admin" : "User"})"
+  end
 end
