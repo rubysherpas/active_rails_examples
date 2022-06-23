b883ca61506efeb7807395aa9c6593866e7e6047
Ryan Bigg <me@ryanbigg.com>
2022-06-23 20:10:59 +1000

    Redirect the users back to the projects page if they  try going to a project that does not exist

--- ticketee/app/controllers/projects_controller.rb
+++ ticketee/app/controllers/projects_controller.rb
@@ -1,4 +1,6 @@
 class ProjectsController < ApplicationController
+  before_action :set_project, only: %i(show edit update destroy)
+
   def index
     @projects = Project.all
   end
@@ -48,6 +50,13 @@ class ProjectsController < ApplicationController
   end
 
   private
+
+  def set_project
+    @project = Project.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    flash[:alert] = "The project you were looking for could not be found."
+    redirect_to projects_path
+  end
 
   def project_params
     params.require(:project).permit(:name, :description)
