396d513b223afddd60012ec94fa115cfc0dfc653
Ryan Bigg <me@ryanbigg.com>
2022-06-28 09:37:41 +1000

    Added devise + sign up feature

--- ticketee/db/schema.rb
+++ ticketee/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_23_101620) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_27_232136) do
   create_table "projects", force: :cascade do |t|
     t.string "name"
     t.string "description"
@@ -25,6 +25,18 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_23_101620) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["project_id"], name: "index_tickets_on_project_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "tickets", "projects"
